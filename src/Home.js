f899a42b68c50ae088e560fc9050b2206504e2b0
jenniferksantos <jenniferksantos@outlook.com>
2023-12-17 14:01:51 +0000

    New feature added

--- src/Home.js
+++ src/Home.js
@@ -10,21 +10,21 @@ function App() {
         <Header />
         <main>
         <section className="home-section">
-            <h2><b>So you think you know ReactJS?</b> Take the 10 quizzes below to test your knowledge.</h2>
+            <h2><b>So you think you really know ReactJS?</b> Take the 10 quizzes below to test your knowledge.</h2>
             <div className="two-cols">
             <div>
             <p><b>1</b> - &nbsp;Functional Components and Props: <Link to="/quiz-questions/1">Start</Link> </p>
-            <p><b>2</b> - &nbsp;JavaScript Basics and Data Types: <Link to="/quiz-questions/2">Start</Link> </p>
-            <p><b>3</b> - &nbsp;JavaScript Basics and Fundamental Concepts: <Link to="/quiz-questions/1">Start</Link> </p>
-            <p><b>4</b> - &nbsp;JavaScript Basics and Data Manipulation: <Link to="/quiz-questions/2">Start</Link> </p>
-            <p><b>5</b> - &nbsp;JavaScript Basics and DOM Manipulation: <Link to="/quiz-questions/1">Start</Link> </p>
+            <p><b>2</b> - &nbsp;State Management in React: <Link to="/quiz-questions/2">Start</Link> </p>
+            <p><b>3</b> - &nbsp;React Hooks: <Link to="/quiz-questions/1">Start</Link> </p>
+            <p><b>4</b> - &nbsp;Forms and Form Handling: <Link to="/quiz-questions/2">Start</Link> </p>
+            <p><b>5</b> - &nbsp;Lifecycle Methods: <Link to="/quiz-questions/1">Start</Link> </p>
             </div>
             <div>
-            <p><b>6</b> - &nbsp;JavaScript Basics and Common Methods: <Link to="/quiz-questions/2">Start</Link> </p>
-            <p><b>7</b> - &nbsp;JavaScript Basics and Duplicate Questions Handling: <Link to="/quiz-questions/1">Start</Link> </p>
-            <p><b>8</b> - &nbsp;JavaScript Basics and Practices: <Link to="/quiz-questions/2">Start</Link> </p>
-            <p><b>9</b> - &nbsp;JavaScript Basics and Comprehensive Review: <Link to="/quiz-questions/2">Start</Link> </p>
-            <p><b>10</b> - &nbsp;JavaScript Essentials Mastery: <Link to="/quiz-questions/2">Start</Link> </p>
+            <p><b>6</b> - &nbsp;Context API: <Link to="/quiz-questions/2">Start</Link> </p>
+            <p><b>7</b> - &nbsp;Higher-Order Components (HOC): <Link to="/quiz-questions/1">Start</Link> </p>
+            <p><b>8</b> - &nbsp;Testing in React: <Link to="/quiz-questions/2">Start</Link> </p>
+            <p><b>9</b> - &nbsp;Routing in React: <Link to="/quiz-questions/2">Start</Link> </p>
+            <p><b>10</b> - &nbsp;Server-Side Rendering (SSR) with React: <Link to="/quiz-questions/2">Start</Link> </p>
             </div>
             </div>
 
